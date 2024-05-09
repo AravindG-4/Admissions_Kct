bebf18a20ea119a0b7df369a4fb75059935081fd
AravindG-4 <aravindganesan492004@gmail.com>
2024-05-09 22:59:38 +0530

    final_update

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, request, jsonify
-import main 
+from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/')
 def index():
     return jsonify({'message': 'Hello, World!'}), 200
